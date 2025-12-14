aa56a351b677597e4b3a242373787c04117aaa3e
Przemysław Graczyk <przemyslawgraczyk@outlook.com>
2025-12-14 21:26:54 +0100

    Initial Commit

---
+++ cpp_intro/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_intro
+//
+//  Created by Przemysław Graczyk on 14/12/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
